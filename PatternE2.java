9b7d26042887a8fd594504ee93efcb15fdc91913
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2022-09-21 23:19:58 +0530

    Update and rename Pattern.java to PatternE2.java

--- PatternE2.java
+++ PatternE2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pattern{
+public class PatternE2{
 	public static void main(String[] args){
 		Scanner sc  = new Scanner(System.in);
 		System.out.print("Enter the number of rows: ");
