3e7fdb2bfa3945c2d19472205cf4cb81cc8f385d
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-01-16 19:28:17 +0530

    Create Substrings E8.java

---
+++ Substrings E8.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Substrings{
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    System.out.pritnln("indexOf(t) = "+s.indexOf('t'));
+    System.out.println("lastIndexOf(t) ="+s.lastIndexOf('t'));
+  }
+}
