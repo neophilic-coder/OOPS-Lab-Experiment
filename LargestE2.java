951f8c004208c85053e4fc030739d62dae5e2c06
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-01-17 18:35:43 +0530

    Update LargestE2.java

--- LargestE2.java
+++ LargestE2.java
@@ -5,21 +5,22 @@ public class LargestE2
 		
 		int x, y, z;
         
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter the first number:");
-        x = s.nextInt();
-        System.out.print("Enter the second number:");
-        y = s.nextInt();
-        System.out.print("Enter the third number:");
-        z = s.nextInt();
+    Scanner s = new Scanner(System.in);
+    System.out.print("Enter the first number:");
+    x = s.nextInt();
+    System.out.print("Enter the second number:");
+    y = s.nextInt();
+    System.out.print("Enter the third number:");
+    z = s.nextInt();
         
-        if(x>y && x>z){
-            System.out.println("The greatest number is:"+x);
-        }
-        else if(y>z && y>x){
-            System.out.println("The greatest number is:"+y);
-        }if(z>y && z>x){
-            System.out.println("The greatest number is:"+z);
-        }
+    if(x>y && x>z){
+       System.out.println("The greatest number is:"+x);
+    }
+    else if(y>z && y>x){
+        System.out.println("The greatest number is:"+y);
+    }
+		else if(z>y && z>x){
+        System.out.println("The greatest number is:"+z);
+    }
 	}
 }
